03b749e599932a06c92d934d17eac3defef5182e
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-06 22:28:44 -0500

    Add Ids_calculator.py with predefined IDs

---
+++ modules/catalogo_citaciones/Ids_calculator.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""IDs de calculator. Solo ids."""
+
+IDS = [
+    # archivos / módulo
+    "calculator",
+    "coherencia",
+    "logica",
+    "correlacion_k",
+    "conteos",
+    "escalas_ids",
+
+    # factores
+    "C",
+    "L",
+    "K",
+
+    # apis
+    "calcular_c",
+    "calcular_l",
+    "calcular_k",
+    "extraer_conteos",
+    "inyectar_en_peticion",
+    "leer_ids_escala",
+
+    # escalas
+    "tru_atomo",
+    "tru_frase",
+    "tru_sujeto",
+    "tru_conversacion",
+    "tru_repositorio",
+
+    # claves de conteo
+    "compromisos",
+    "contradicciones",
+    "posturas",
+    "reversiones",
+    "afirmaciones",
+    "afirmaciones_falsas",
+
+    # bases / pesos
+    "m",
+    "p",
+    "c",
+    "k",
+    "r",
+    "f",
+
+    # meta
+    "base_nula_C",
+    "base_nula_L",
+    "base_nula_K",
+    "o_presente",
+    "O_context",
+    "contexto",
+    "UNDEFINED",
+
+    # métodos
+    "operacional",
+    "teorico",
+
+    # rol
+    "CA",
+]

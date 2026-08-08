92fd802be5ce7384e9330eacc6350d58b6a94487
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-08 16:33:48 -0500

    Fix syntax error by removing trailing comma
    Removed trailing comma in dictionary definition.

--- modules/axiomas/__init__.py
+++ modules/axiomas/__init__.py
@@ -223,7 +223,6 @@ CONTENEDOR: Dict[str, Any] = {
     "estabilidad": ESTABILIDAD,
     "compatible_desde": COMPATIBLE_DESDE,
     "api_engine": API_ENGINE,
-}
 
     # ----- IDENTIDAD -----
     "id": ID_MODULO,

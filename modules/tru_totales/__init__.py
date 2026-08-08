2bd59390fdaf8d1ff38bfef79b7c0b3c817335e4
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-08 07:20:46 -0500

    Add 'reporte' key to configuration dictionary

--- modules/tru_totales/__init__.py
+++ modules/tru_totales/__init__.py
@@ -306,6 +306,7 @@ CONTENEDOR: Dict[str, Any] = {
         "conocimiento": True,
         "metricas": True,
         "diagnostico": True,
+        "reporte": True,
     },
 
     # ----- ESTADOS VÁLIDOS -----

92d1e4a95f4acabad993b4ff61b9ea0166026560
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-08 07:35:44 -0500

    Add 'reporte' key to configuration dictionary

--- modules/cache/__init__.py
+++ modules/cache/__init__.py
@@ -625,6 +625,7 @@ CONTENEDOR: Dict[str, Any] = {
         "conocimiento": True,
         "metricas": True,
         "diagnostico": True,
+        "reporte": True,
     },
 
     # ----- ESTADOS VÁLIDOS -----

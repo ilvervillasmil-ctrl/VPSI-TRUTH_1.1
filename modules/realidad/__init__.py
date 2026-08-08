e9dedbe6f29ebf34cb1df748c7d627d700f759c0
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-08 07:40:37 -0500

    Add 'reporte' key to configuration dictionary

--- modules/realidad/__init__.py
+++ modules/realidad/__init__.py
@@ -341,6 +341,7 @@ CONTENEDOR: Dict[str, Any] = {
         "conocimiento": True,
         "metricas": True,
         "diagnostico": True,
+        "reporte": True,
     },
 
     # ----- ESTADOS VÁLIDOS -----

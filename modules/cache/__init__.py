b13fda0ed98e57b5691bf7de1e048faf39becd45
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-12 15:45:52 -0500

    Update dependencies list in __init__.py

--- modules/cache/__init__.py
+++ modules/cache/__init__.py
@@ -457,7 +457,10 @@ CONTENEDOR: Dict[str, Any] = {
     # ============================================================
     # DEPENDENCIAS
     # ============================================================
-    "requiere": ["*"],
+    "requiere": ["CT", "AX", "FO", "MC", 
+                        "SF", "CA", "CX", "CC",
+                        "DI", "RE", "VX", "TX", 
+                        "CH", "CIT", "TT", "CE",],
 
     # ============================================================
     # ACCESO A ARCHIVOS (AGREGADO — obligatorio en el esquema)
